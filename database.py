02be090d15489d583bb3f86b884949121eb1d3c5
sash00k <s.a.c.h.a.b@yandex.ru>
2022-12-15 15:14:43 +0700

    8 tests passed

--- database.py
+++ database.py
@@ -59,7 +59,7 @@ class Teacher(Person):
         self.students = students
 
     def __str__(self):
-        return f'Teacher {self.name} on course {self.course}. Works' \
+        return f'Teacher {self.name} on course {self.course}. Works ' \
         f'with {len(self.groups)} groups and {len(self.students)} students.'
 
 class AssistantStudent(Student, Teacher, Person):
@@ -68,7 +68,7 @@ class AssistantStudent(Student, Teacher, Person):
         super().__init__(name, birthday, person_id, department, group, student_id, course, groups, students)
 
     def __str__(self):
-        return f'Student {self.name} from group {self.group}, {self.department}. Also teaches' \
+        return f'Student {self.name} from group {self.group}, {self.department}. Also teaches ' \
                f'{self.course} in {len(self.groups)} groups and mentors {len(self.students)} students.'
 
 def get_entities(db: TextIO) -> str:
@@ -83,21 +83,20 @@ def get_entities(db: TextIO) -> str:
             entity_class = AssistantStudent
         else:
             raise ValueError(f'Incorrect record in database: {dict_length} fields get')
+        init_dict['person_id'] = init_dict.pop('id')
+        # entity = entity_class.init_from_json(init_dict)
+        # print(entity_class, entity)
         yield entity_class.init_from_json(init_dict)
 
 def just_read(db: TextIO, output: TextIO) -> None:
     for entity in get_entities(db):
         print(entity, file=output)
 
-def field_is_set(field: str, db: TextIO, output: TextIO) -> None:
-    for entity in get_entities(db):
-        if hasattr(entity, field):
-            print(entity, file=output)
-
 def field_is_arg(field: str, arg, db: TextIO, output: TextIO) -> None:
     for entity in get_entities(db):
-        if str(getattr(entity, field)) == arg:
-            print(entity, file=output)
+        if hasattr(entity, field):
+            if arg in (str(getattr(entity, field)), 'set'):
+                print(entity, file=output)
 
 def field_in_args(field: str, args: list, db: TextIO, output: TextIO) -> None:
     collection = set(map(lambda x: x.strip('{}", '), args))
@@ -114,20 +113,19 @@ def solution(requests: TextIO, db_name: str, output: TextIO) -> None:
     for request in requests.readlines():
         with open(db_name, 'r') as db:
             tokens = request.split()
-            print(len(tokens))
-            field, cond, args = ('', '<no commands>', [''])
+            if len(tokens) < 3:
+                cond = '<no commands>'
+            else:
+                field, cond, args = tokens[3], tokens[4], tokens[5:]
             try:
-                match cond, args[0]:
-                    case '<no commands>', _:
-                        print('kek')
+                match cond:
+                    case '<no commands>':
                         just_read(db=db, output=output)
-                    case 'is', 'set':
-                        field_is_set(field=field, db=db, output=output)
-                    case 'is', _:
+                    case 'is':
                         field_is_arg(field=field, arg=args[0], db=db, output=output)
-                    case 'in', _:
+                    case 'in':
                         field_in_args(field=field, args=args, db=db, output=output)
-                    case 'contains', _:
+                    case 'contains':
                         field_contains_arg(field=field, arg=args[0], db=db, output=output)
                     case _:
                         print(f'unknown command "{request}"', file=output)
