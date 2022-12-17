003281d02a0d527ea4f337a622eda4332f4d666b
sash00k <s.a.c.h.a.b@yandex.ru>
2022-12-18 01:32:35 +0700

    fix solution logic

--- database.py
+++ database.py
@@ -71,71 +71,78 @@ class AssistantStudent(Student, Teacher, Person):
         return f'Student {self.name} from group {self.group}, {self.department}. Also teaches ' \
                f'{self.course} in {len(self.groups)} groups and mentors {len(self.students)} students.'
 
-def get_entities(db: TextIO) -> str:
-    for record in db.readlines():
-        init_dict = json.loads(record)
-        dict_length = len(init_dict)
-        if dict_length == 3:
-            entity_class = Person
-        elif dict_length == 6:
-            entity_class = Teacher if 'students' in init_dict else Student
-        elif dict_length == 9:
-            entity_class = AssistantStudent
-        else:
-            raise ValueError(f'Incorrect record in database: {dict_length} fields get')
-        init_dict['person_id'] = init_dict.pop('id')
-        # # LOGS
-        # entity = entity_class.init_from_json(init_dict)
-        # print(entity_class, entity)
-        yield entity_class.init_from_json(init_dict)
+def get_entities(db_name: str) -> str:
+    with open(db_name, 'r') as db:
+        for record in db.readlines():
+            init_dict = json.loads(record)
+            dict_length = len(init_dict)
+            if dict_length == 3:
+                entity_class = Person
+            elif dict_length == 6:
+                entity_class = Teacher if 'students' in init_dict else Student
+            elif dict_length == 9:
+                entity_class = AssistantStudent
+            else:
+                raise ValueError(f'Incorrect record in database: {dict_length} fields get')
+            init_dict['person_id'] = init_dict.pop('id')
+            # # LOGS
+            # entity = entity_class.init_from_json(init_dict)
+            # print(entity_class, entity)
+            yield entity_class.init_from_json(init_dict)
 
-def just_read(db: TextIO, output: TextIO) -> None:
-    for entity in get_entities(db):
-        print(entity, file=output)
-
-def field_is_arg(field: str, arg: str, db: TextIO, output: TextIO) -> None:
+def field_is_arg(field: str, arg: str, db_name: str):
     arg = arg.strip('"')
-    for entity in get_entities(db):
+    for entity in get_entities(db_name):
         if hasattr(entity, field):
             if arg in (str(getattr(entity, field)), 'set'):
-                print(entity, file=output)
+                yield entity
+            else:
+                del entity
 
-def field_in_args(field: str, args: list, db: TextIO, output: TextIO) -> None:
+def field_in_args(field: str, args: list, db_name: str):
     collection = set(map(lambda x: x.strip('{}", '), args))
-    for entity in get_entities(db):
+    for entity in get_entities(db_name):
         if hasattr(entity, field) and str(getattr(entity, field)) in collection:
-            print(entity, file=output)
+            yield entity
+        else:
+            del entity
 
-def field_contains_arg(field: str, arg, db: TextIO, output: TextIO) -> None:
+def field_contains_arg(field: str, arg, db_name: str):
     arg = arg.strip('"')
-    for entity in get_entities(db):
+    for entity in get_entities(db_name):
         if hasattr(entity, field) and str(arg) in str(getattr(entity, field)):
-            print(entity, file=output)
+            yield entity
+        else:
+            del entity
+
+def proceed_request(request: str, db_name: str):
+    try:
+        tokens = request.split()
+        if tokens == ['get', 'records']:
+            cond = '<no commands>'
+        else:
+            field, cond, args = tokens[3], tokens[4], tokens[5:]
+        match cond:
+            case '<no commands>':
+                return get_entities(db_name=db_name)
+            case 'is':
+                return field_is_arg(field=field, arg=args[0], db_name=db_name)
+            case 'in':
+                return field_in_args(field=field, args=args, db_name=db_name)
+            case 'contains':
+                return field_contains_arg(field=field, arg=args[0], db_name=db_name)
+            case _:
+                print(f'unknown command "{request}"')
+    except ValueError as invalid_input:
+        print(f'Invalid input: {invalid_input}')
+    except IndexError as invalid_input:
+        print(f'Invalid input (maybe command is too short): {invalid_input}')
+
 
 def solution(requests: TextIO, db_name: str, output: TextIO) -> None:
     for request in requests.readlines():
-        with open(db_name, 'r') as db:
-            try:
-                tokens = request.split()
-                if tokens == ['get', 'records']:
-                    cond = '<no commands>'
-                else:
-                    field, cond, args = tokens[3], tokens[4], tokens[5:]
-                match cond:
-                    case '<no commands>':
-                        just_read(db=db, output=output)
-                    case 'is':
-                        field_is_arg(field=field, arg=args[0], db=db, output=output)
-                    case 'in':
-                        field_in_args(field=field, args=args, db=db, output=output)
-                    case 'contains':
-                        field_contains_arg(field=field, arg=args[0], db=db, output=output)
-                    case _:
-                        print(f'unknown command "{request}"', file=output)
-            except ValueError as invalid_input:
-                print(f'Invalid input: {invalid_input}')
-            except IndexError as invalid_input:
-                print(f'Invalid input (maybe command is too short): {invalid_input}')
+        for entity in proceed_request(request=request, db_name=db_name):
+            print(entity, file=output)
 
 
 if __name__ == '__main__':
